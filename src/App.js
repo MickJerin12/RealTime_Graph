e7df0ad09cfc9a62cfeb216f794bf7088da7f324
Mick Jerin <s.mickjerin@outlook.com>
2020-11-12 14:39:05 +0530

    Styling fixes

--- src/App.js
+++ src/App.js
@@ -1,62 +1,84 @@
-import React, { useState } from 'react';
-import './App.css';
-import Chart from './components/Chart';
+import React, { useState } from "react";
+import "./App.css";
+import Chart from "./components/Chart";
 
 const App = () => {
+  const [chartData, setChartData] = useState({
+    labels: [
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+    ],
+    datasets: [
+      {
+        label: "Temperature",
+        data: [
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+        ],
+        borderColor: ["#1b03a3"],
+        backgroundColor: ["transparent"],
+      },
+    ],
+  });
 
-  const [chartData,setChartData]=useState({
-      labels: ['', '', '', '', '', '','','','','','','','','','','','','','','','','','','',''],
-      datasets:[
-        {
-          label:'Temperature',
-          data:[
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-          ],
-        }
-      ]
-    
-  })
-
-
-  setTimeout(()=>{
-    const cloneChartData={...chartData};
-    const cloneData=[...chartData.datasets[0].data];
+  setTimeout(() => {
+    const cloneChartData = { ...chartData };
+    const cloneData = [...chartData.datasets[0].data];
     cloneData.shift();
-    cloneData.push(Math.floor((Math.random() * 90) + 0));
-    cloneChartData.datasets[0].data=cloneData
-    setChartData(cloneChartData)
-  },1000)
+    cloneData.push(Math.floor(Math.random() * 90 + 0));
+    cloneChartData.datasets[0].data = cloneData;
+    setChartData(cloneChartData);
+  }, 500);
 
-    return (
-      <div className="App">
-        <Chart chartData={chartData} location="Massachusetts" legendPosition="bottom"/>
-      </div>
-    );
-
-}
+  return (
+    <div className="App">
+      <Chart
+        chartData={chartData}
+        location="Massachusetts"
+        legendPosition="bottom"
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,53 +1,67 @@
-import React from 'react';
-import { Line} from 'react-chartjs-2';
+import React from "react";
+import { Line } from "react-chartjs-2";
 
-const Chart =({chartData})=>{
-
-    return (
-<div>
-        <Line
-          data={chartData}
-          options={{
-            title:{
-              display:"Test Graph",
-              fontSize:25
-            },
-            scales: {
-              xAxes: [{
-                  ticks: {
-                      display: false 
-                  }
-              }],
-              yAxes: [{
+const Chart = ({ chartData }) => {
+  return (
+    <div>
+      <Line
+        data={chartData}
+        responsive={true}
+        maintainAspectRatio={false}
+        options={{
+          title: {
+            display: "Test Graph",
+            fontSize: 25,
+          },
+          scales: {
+            xAxes: [
+              {
+                gridLines: {
+                  drawTicks: false,
+                  color: ["rgb(0, 0, 0)"],
+                },
                 ticks: {
-                    suggestedMin: 0,
-                    suggestedMax: 100
-                }
-            }]
+                  display: false,
+                },
+              },
+            ],
+            yAxes: [
+              {
+                gridLines: {
+                  color: ["rgb(0, 0, 0)"],
+                },
+                ticks: {
+                  suggestedMin: 0,
+                  suggestedMax: 100,
+                },
+              },
+            ],
           },
           animation: {
-            duration: 0
-        },
-        hover: {
-          animationDuration: 0
-      },
-      responsiveAnimationDuration: 0,
-    legend: {
-      display: false
-   },
-   tooltips: {
-      enabled: false
-   },
-      elements: {
-        point:{
-          radius: 0
-      },
-     
-    }
-          }}
-        />
-      </div>
-    )
-}
+            duration: 0,
+          },
+          hover: {
+            animationDuration: 0,
+          },
+          responsiveAnimationDuration: 0,
+          legend: {
+            display: false,
+          },
+          tooltips: {
+            enabled: false,
+          },
+          elements: {
+            point: {
+              radius: 0,
+            },
+            //   line: {
+            //     tension: 0
+            // }
+          },
+        }}
+      />
+    </div>
+  );
+};
 
 export default Chart;
